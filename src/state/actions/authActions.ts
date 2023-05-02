2933dd72dd6f98a60825baebb8ff2565b9fcd3ff
Sergio Cabrera <sergiocabreranieto@gmail.com>
2023-05-01 19:54:17 -0500

    all deleted

--- src/state/actions/authActions.ts
+++
@@ -1,9 +0,0 @@
-/*Los Actions son funciones que devuelven objetos planos.
-Una acción debe tener una propiedad type que indica el tipo de acción a realizar.
-Los tipos normalmente son definidos como strings constantes
-*/
-
-
-//en este archivo se definen las acciones para el user: loggearse y cerrar sesión
-
-export type AuthActions = {type:'login', token: string} | {type:'logout'};
