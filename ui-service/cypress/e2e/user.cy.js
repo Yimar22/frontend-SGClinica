74766491d105f889ef108cb715a879ecd7937670
Dtorres1ro <46894535+Datorresp@users.noreply.github.com>
2023-06-05 22:10:36 -0500

    Create Login Testing (#2)

--- ui-service/cypress/e2e/user.cy.js
+++ ui-service/cypress/e2e/user.cy.js
@@ -1,5 +1,28 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000')
-  })
+describe('Verifying Login Process ', () => {
+
+    beforeEach(() => {
+        cy.clearCookies();
+        cy.visit("http://localhost:3000");
+      });
+
+    it("Login should notify when a wrong username/password combination is used.", () => {
+        cy.get('#username').type("testUser");
+        cy.get('#password').type("testPass");
+        cy.get('.btn').click();
+
+        // Assertion for error message
+        cy.get('.fade').should(
+          "include.text",
+          "Usuario no encontrado")
+    });
+
+    it("Login should work for existing user.", () => {
+
+        cy.get('#username').type("admin");
+        cy.get('#password').type("testpassword");
+        cy.get('.btn').click();
+        cy.get('.navbar-toggler-icon').click();
+        // Assertion that verifies that products list is displayed
+        cy.get(':nth-child(1) > #offcanvasNavbarDropdown-expand-false').should("have.text", "Gestión de Usuarios")
+    });
 })
